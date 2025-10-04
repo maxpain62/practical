0344cfb7be45412ca92b64bfec36f2f7e60ed0be
maxpain62 <maxpain62@gmail.com>
2025-10-04 13:13:05 +0530

    minor changes in terraform ec2

--- terraform/EC2/simple-ec2/ec2.tf
+++ terraform/EC2/simple-ec2/ec2.tf
@@ -12,13 +12,21 @@ provider "aws" {
 }
 
 resource "aws_instance" "simple_ec2" {
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   ami = "ami-02d26659fd82cf299"
   security_groups = ["launch-wizard-1"]
   key_name = "dpp-key"
   iam_instance_profile = "terraform_role"
+  instance_market_options {
+    market_type = "spot"
+  }
+  user_data = file("terraform_installation.sh")
   tags = {
     Name = "simple_ec2"
     env = "dev"
   }
+}
+
+output "simple_ec2_public_ip" {
+  value = aws_instance.simple_ec2.public_ip
 }
