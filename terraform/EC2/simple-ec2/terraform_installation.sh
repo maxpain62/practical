5b5d3e5d4f807375dbbf22b8d7aa2032e9b63218
maxpain62 <maxpain62@gmail.com>
2025-10-04 13:12:45 +0530

    terraform installation script for userdata

---
+++ terraform/EC2/simple-ec2/terraform_installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#user data script from terraform installation
+apt-get update && apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+apt update; sudo apt-get install terraform -y
+
+cd ~; git clone https://github.com/maxpain62/practical.git
