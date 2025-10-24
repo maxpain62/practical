cbf42fb42191e71e9a30117fe417c7fad9af7d79
maxpain62 <maxpain62@gmail.com>
2025-10-24 22:44:38 +0530

    folder renamed

--- terraform/eks/00_backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "testbkt17062025"
-    key          = "sample_eks/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-  }
-}
