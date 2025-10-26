9dd2ae2339d6f9495bf6301f7bec8799f05594af
maxpain62 <maxpain62@gmail.com>
2025-10-26 17:56:27 +0530

    file formating

--- terraform/01-EC2/simple-ec2/terraform_installation.sh
+++ terraform/01-EC2/simple-ec2/terraform_installation.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-#user data script from terraform installation
+#user data script from terraform, aws cli and kubectl installation
 apt-get update && apt-get install -y gnupg software-properties-common python3.12-venv unzip
-
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
 apt update; sudo apt-get install terraform -y
 
 cd ~; git clone https://github.com/maxpain62/practical.git
 
 ln -s /usr/bin/python3 /usr/bin/python
-
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo /root/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.9/2025-09-19/bin/linux/amd64/kubectl
 chmod +x /root/kubectl
-
 cp -p /root/kubectl /usr/local/bin/
-
 
 EksDir=/root/practical/terraform/02-eks
 
