ea27a4018501e9e8905d81007c0adac06622d452
maxpain62 <maxpain62@gmail.com>
2025-11-09 18:13:42 +0530

    files moved and added new practice folder

---
+++ terraform/practice/01-basic/01-ec2/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+        version = "~ > 6.0"
+        source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "tf-demo-ec2" {
+  instance_type = "t2.micro"
+  ami = "ami-02d26659fd82cf299"
+  security_groups = [ "launch-wizard" ]
+  key_name = "dpp-key"
+}
