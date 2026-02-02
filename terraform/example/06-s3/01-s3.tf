30b27d39b345ece19d22933f4dce442d3e1279c7
maxpain62 <maxpain62@gmail.com>
2026-02-02 09:12:47 +0530

    added aws_s3_bucket_public_access_block

--- terraform/example/06-s3/01-s3.tf
+++ terraform/example/06-s3/01-s3.tf
@@ -5,3 +5,9 @@ resource "aws_s3_bucket" "eos_s3_bubket" {
     env = "dev"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "eos_s3_bubket_public_access" {
+  bucket = aws_s3_bucket.eos_s3_bubket.id
+  block_public_acls   = false
+  block_public_policy = false
+}
