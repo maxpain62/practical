aef6a9ea599910ed7e6a754b23e1bd0eb5d89e21
maxpain62 <maxpain62@gmail.com>
2026-02-02 09:02:03 +0530

    s3 name change

--- terraform/example/06-s3/01-s3.tf
+++ terraform/example/06-s3/01-s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "eos_s3_bubket" {
-  bucket = "eos_s3_bubket"
+  bucket = "eos-s3-bubket"
   tags = {
-    Name = "eos_s3_bubket"
+    Name = "eos-s3-bubket"
     env = "dev"
   }
 }
