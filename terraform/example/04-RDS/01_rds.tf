bb5339b224e8566019f0df23458de390371ec0ad
maxpain62 <maxpain62@gmail.com>
2026-02-05 15:40:27 +0530

    changed db_name

--- terraform/example/04-RDS/01_rds.tf
+++ terraform/example/04-RDS/01_rds.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "eos_rds" {
   allocated_storage = 20
   max_allocated_storage = 25
-  db_name = "eos_db"
+  db_name = "edwiki"
   engine  = "mariadb"
   engine_version = "10.11.15"
   instance_class = "db.t3.micro"
