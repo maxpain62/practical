effd4e7aeca57d5992f33ff458d47b660e61b080
maxpain62 <maxpain62@gmail.com>
2026-02-09 10:20:57 +0530

    changed instance class to db.t3.medium

--- terraform/example/04-RDS/01_rds.tf
+++ terraform/example/04-RDS/01_rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "eos_rds" {
   db_name = "edwiki"
   engine  = "mariadb"
   engine_version = "10.11.15"
-  instance_class = "db.t3.micro"
+  instance_class = "db.t3.medium"
   backup_retention_period = 0
   availability_zone = "ap-south-1a"
   username = "eosadmin"
