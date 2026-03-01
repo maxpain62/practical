58057249b5cb3dbe7bc812aa43f61da50352b993
maxpain62 <maxpain62@gmail.com>
2026-03-01 07:18:02 +0530

    Refactor formatting for consistency in network and resource group Terraform configurations

--- terraform/azure/aks/network.tf
+++ terraform/azure/aks/network.tf
@@ -1,16 +1,16 @@
 resource "azurerm_network_security_group" "tf-demo-nsg" {
-  name = "tf-demo-nsg"
-  location = azurerm_resource_group.tf-demo-rg.location
+  name                = "tf-demo-nsg"
+  location            = azurerm_resource_group.tf-demo-rg.location
   resource_group_name = azurerm_resource_group.tf-demo-rg.name
   security_rule {
-    name = "SSH"
-    priority = 100
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "TCP"
-    source_port_range = "*"
-    destination_port_range = "*"
-    source_address_prefix = "*"
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "TCP"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
   tags = {
@@ -19,10 +19,10 @@ resource "azurerm_network_security_group" "tf-demo-nsg" {
 }
 
 resource "azurerm_virtual_network" "tf-demo-vnet" {
-  name = "tf-demo-vnet"
-  location = azurerm_resource_group.tf-demo-rg.location
+  name                = "tf-demo-vnet"
+  location            = azurerm_resource_group.tf-demo-rg.location
   resource_group_name = azurerm_resource_group.tf-demo-rg.name
-  address_space = [ "10.0.0.0/16" ]
+  address_space       = ["10.0.0.0/16"]
 
   tags = {
     env = "dev"
@@ -30,9 +30,9 @@ resource "azurerm_virtual_network" "tf-demo-vnet" {
 }
 
 resource "azurerm_subnet" "tf-demo-subnet1" {
-  name = "tf-demo-subnet1"
-  resource_group_name = azurerm_resource_group.tf-demo-rg.name
-  address_prefixes = [ "10.0.1.0/24" ]
+  name                 = "tf-demo-subnet1"
+  resource_group_name  = azurerm_resource_group.tf-demo-rg.name
+  address_prefixes     = ["10.0.1.0/24"]
   virtual_network_name = azurerm_virtual_network.tf-demo-vnet.name
 
   tags = {
--- terraform/azure/aks/resource-group.tf
+++ terraform/azure/aks/resource-group.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "tf-demo-rg" {
   location = "Central India"
-  name = "tf-demo-rg"
+  name     = "tf-demo-rg"
 }
