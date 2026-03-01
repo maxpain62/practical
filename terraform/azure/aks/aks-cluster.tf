b935598ec0b0d4f5086fd2408827e9f82e2ba350
maxpain62 <maxpain62@gmail.com>
2026-03-01 08:58:40 +0530

    added azurerm_kubernetes_cluster resource and infra-controller_key for AKS setup

---
+++ terraform/azure/aks/aks-cluster.tf
@@ -0,0 +1,32 @@
+resource "azurerm_kubernetes_cluster" "tf-demo-aks-cluster" {
+  name = "tf-demo-aks-cluster"
+  location = azurerm_resource_group.tf-demo-rg.location
+  resource_group_name = azurerm_resource_group.tf-demo-rg.name
+  oidc_issuer_enabled = true
+  sku_tier = "Free"
+  support_plan = "KubernetesOfficial"
+
+  default_node_pool {
+    name = "tf-demo-default-nodepool"
+    node_count = 3
+    vm_size = "Standard_B2as_v2"
+    vnet_subnet_id = azurerm_subnet.tf-demo-subnet1.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  
+  linux_profile {
+    admin_username = "azureadmin"
+    
+    ssh_key {
+      key_data = file("./infra-controller_key")
+    }
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+}
