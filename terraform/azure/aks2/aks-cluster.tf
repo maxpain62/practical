03d79f1067d6c7fd8882c67d4c84c7d685f36d74
maxpain62 <maxpain62@gmail.com>
2026-03-04 18:18:32 +0530

    terraform configuration for aks experimental purpose

---
+++ terraform/azure/aks2/aks-cluster.tf
@@ -0,0 +1,34 @@
+resource "azurerm_kubernetes_cluster" "aks-demo-cluster" {
+  name                = "aks-demo-cluster"
+  location            = azurerm_resource_group.aks-demo-rg.location
+  resource_group_name = azurerm_resource_group.aks-demo-rg.name
+  dns_prefix          = "aksdemo"
+  oidc_issuer_enabled = true
+  sku_tier            = "Free"
+  support_plan        = "KubernetesOfficial"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    min_count  = 2
+    max_count  = 4
+    vm_size    = "Standard_B4als_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+
+  linux_profile {
+    admin_username = "azureadmin"
+
+    ssh_key {
+      key_data = file("./infra-controller_key")
+    }
+  }
+}
---
+++ terraform/azure/aks2/resource-group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "aks-demo-rg" {
+  location = "Central India"
+  name     = "aks-demo-rg"
+}
