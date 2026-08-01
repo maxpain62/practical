ed4b2fe7299b21cc8a2c41342c6b1de4e3006359
maxpain62 <maxpain62@gmail.com>
2026-08-01 13:25:33 +0530

    corrected cluster version to 1.33

--- terraform/aws/example/02-eks/02_cluster_nodegroup.tf
+++ terraform/aws/example/02-eks/02_cluster_nodegroup.tf
@@ -21,7 +21,7 @@ resource "aws_launch_template" "eks_demo_launch_template" {
 resource "aws_eks_cluster" "demo_cluster" {
   name     = "demo-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.3"
+  version  = "1.33"
 
   upgrade_policy {
     support_type = "EXTENDED"
