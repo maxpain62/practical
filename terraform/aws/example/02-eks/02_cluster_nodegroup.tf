98c2af31e02eddb7dd22664cdf17fd2032a94b95
maxpain62 <maxpain62@gmail.com>
2026-08-03 07:27:59 +0530

    update EKS cluster version to 1.34 and change upgrade policy support type to STANDARD

--- terraform/aws/example/02-eks/02_cluster_nodegroup.tf
+++ terraform/aws/example/02-eks/02_cluster_nodegroup.tf
@@ -21,10 +21,10 @@ resource "aws_launch_template" "eks_demo_launch_template" {
 resource "aws_eks_cluster" "demo_cluster" {
   name     = "demo-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.33"
+  version  = "1.34"
 
   upgrade_policy {
-    support_type = "EXTENDED"
+    support_type = "STANDARD"
   }
 
   vpc_config {
